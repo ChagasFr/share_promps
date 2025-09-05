a623ab543a9d7660c7afeea46c47a43a351fbe3e
ChagasFr <felipechagasdosreis@gmail.com>
2025-09-05 19:02:00 -0300

    Feat: rotas hello

--- app/page.js
+++ app/page.js
@@ -1,6 +1,19 @@
-async function Page({params}) {
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${params.id})`,
-    {cache: 'no-store'});
-};
-  const post = await res.json();
+import Feed from "@components/Feed";
+
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Discover & Share
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> AI-Powered Prompts</span>
+    </h1>
+    <p className='desc text-center'>
+      Promptopia is an open-source AI prompting tool for modern world to
+      discover, create and share creative prompts
+    </p>
+
+    <Feed />
+  </section>
+);
+
+export default Home;
